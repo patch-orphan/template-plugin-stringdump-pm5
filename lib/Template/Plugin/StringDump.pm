3bb34dc0607647c0ff4201eca2d7f55de82b9ccb
Nick Patch <patch@cpan.org>
2011-12-11 19:23:02 -0500

    new project

---
+++ lib/Template/Plugin/StringDump.pm
@@ -0,0 +1,42 @@
+package Template::Plugin::StringDump;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Template::Plugin::StringDump - String::Dump plugin for TT
+
+=head1 VERSION
+
+This document describes Template::Plugin::StringDump version 0.01.
+
+=head1 SYNOPSIS
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2011 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
