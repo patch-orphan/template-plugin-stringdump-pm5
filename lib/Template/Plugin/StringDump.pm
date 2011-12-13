6f0a423452a8ba09b5291119ecbb59d5c30ad3f8
Nick Patch <patch@cpan.org>
2011-12-12 23:12:11 -0500

    release 0.02

--- lib/Template/Plugin/StringDump.pm
+++ lib/Template/Plugin/StringDump.pm
@@ -6,7 +6,7 @@ use warnings;
 use parent qw( Template::Plugin::Filter );
 use String::Dump;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 our $DYNAMIC = 1;  # dynamic filter
 
 use constant FILTER_NAME => 'dump';
@@ -39,7 +39,7 @@ Template::Plugin::StringDump - String::Dump plugin for TT
 
 =head1 VERSION
 
-This document describes Template::Plugin::StringDump version 0.01.
+This document describes Template::Plugin::StringDump version 0.02.
 
 =head1 SYNOPSIS
 
